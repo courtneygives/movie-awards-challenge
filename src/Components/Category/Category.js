c3b1c5f966bf0d16ffbe657bada57d6cc9ce02a4
cgives <cgives@me.com>
2021-01-07 22:53:40 -0600

    get vote selection working

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,4 +1,5 @@
 import React, { useState} from 'react';
+import './Category.css';
 import Nominee from '../Nominee/Nominee';
 
 const Category = (props) => {
--- src/Components/Nominee/Nominee.js
+++ src/Components/Nominee/Nominee.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Nominee.css'
 
 const Nominee = (props) => {
-
+let className = 'nominee'
+if (props.isSelected) {
+  className = 'nominee selected'
+}
   return (
-    <div className='nominee' id={props.id}>
+    <div className={className} id={props.id}>
       <h2>{props.title}</h2>
       <img src={props.photoUrL} alt={props.id} />
+      <button onClick={() => props.setSelected(props.id)}>
+       Vote
+     </button>
     </div>
   )
 }
