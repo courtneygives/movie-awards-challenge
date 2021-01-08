546d55fb216fb4037d24ae7036b34f8e6afd9627
cgives <cgives@me.com>
2021-01-07 20:03:50 -0600

    get Ballot.js to print all the data from Api.js

--- src/Components/Ballot/Ballot.js
+++ src/Components/Ballot/Ballot.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Api from '../../Api/Api';
 
 const Ballot = () => {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    Api.getBallotData().then((data) => setData(data))
+  });
+
   return (
     <div className='ballot'>
       'Your Code Goes Here'
+      {JSON.stringify(data)}
     </div>
   )
 }
